109c720b9f5e4751329116a118d00669a2f80335
Alexei Matyushkin <am@mudasobwa.ru>
2014-01-23 08:31:35 +0400

    Admin removal: continued.

--- config/apps.rb
+++ config/apps.rb
@@ -34,5 +34,4 @@ end
 
 # Mounts the core application for this project
 
-#Padrino.mount("QipowlDemo::Admin", :app_file => Padrino.root('admin/app.rb')).to("/admin")
-#Padrino.mount('QipowlDemo::App', :app_file => Padrino.root('app/app.rb')).to('/')
+Padrino.mount('QipowlDemo::App', :app_file => Padrino.root('app/app.rb')).to('/')
